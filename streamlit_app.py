eca7f18f1aeb3fb7921f79292c074344b10d297f
TonyRitchie <tony.ritchie@planittesting.com>
2026-09-11 10:57:28 +1000

    Comment out dataframe display in streamlit_app.py
    Comment out dataframe display and stop calls.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,12 @@ st.write ("The current Smoothie order is for: ",name_on_order)
 cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, width='stretch')
-st.stop()
+#st.dataframe(data=my_dataframe, width='stretch')
+#st.stop()
 
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 
 ingredients_list = st.multiselect(
